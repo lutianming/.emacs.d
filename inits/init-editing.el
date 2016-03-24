f273320895f22533906996c1329676808488fe87
Tianming LU <tianming@tianming-pc.transwarp.io>
2016-03-24 19:39:56 +0800

    update

--- inits/init-editing.el
+++ inits/init-editing.el
@@ -6,9 +6,6 @@
 (global-set-key (kbd "<f5>") 'find-file) ; Open file or dir
 (global-set-key (kbd "<f6>") 'kill-this-buffer) ; Close file
 
-(global-set-key (kbd "C-w") 'backward-kill-word)
-(global-set-key (kbd "C-x k") 'kill-region)
-(global-set-key (kbd "C-c k") 'kill-ring-save)
 (global-set-key (kbd "C-.") 'undo-tree-undo)
 (global-set-key (kbd "C-/") 'undo-tree-redo)
 
@@ -18,7 +15,6 @@
                   (join-line -1)))
 
 (recentf-mode 1)
-(global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
 (setq auto-save-file-name-transforms
--- inits/init-helm.el
+++ inits/init-helm.el
@@ -26,6 +26,7 @@
 
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
 
 (setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
 (global-set-key (kbd "M-x") 'helm-M-x)
